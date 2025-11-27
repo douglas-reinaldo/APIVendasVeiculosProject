71d436dda057ddeea70483303fadc5272c2402af
Douglas Reinaldo <Douglas39510@gmail.com>
2025-11-27 11:40:48 -0300

    Estrutura inicial do projeto e arquivos de configuração

---
+++ src/main/java/com/example/minha_api_vendas/MinhaApiVendasApplication.java
@@ -0,0 +1,13 @@
+package com.example.minha_api_vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhaApiVendasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhaApiVendasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/minha_api_vendas/MinhaApiVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.minha_api_vendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhaApiVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
