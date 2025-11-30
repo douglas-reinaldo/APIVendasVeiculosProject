7a0385910c8fc8c25697bbcf270e6f741c77ad1e
Douglas Reinaldo <Douglas39510@gmail.com>
2025-11-29 23:39:23 -0300

    feat: Finaliza model Vendedor com relacionamentos e validações (Bean Validation)

--- src/main/java/com/example/minha_api_vendas/model/Vendedor.java
+++ src/main/java/com/example/minha_api_vendas/model/Vendedor.java
@@ -2,6 +2,7 @@ package com.example.minha_api_vendas.model;
 
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.ArrayList;
@@ -14,12 +15,15 @@ public class Vendedor {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "O nome é obrigatorio")
     @Column(nullable = false, length = 50)
     private String nome;
 
+    @NotBlank(message = "O email é obrigatorio")
     @Column(nullable = false, unique = true, length = 50)
     private String email;
 
+    @NotBlank(message = "O telefone é obrigatorio")
     @Column(nullable = false, unique = true, length = 15)
     private String telefone;
 
